c5acb19cc2f9ba180d47e606276b82e369601861
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2024-03-05 21:05:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './component/NavBar';
+import SignUp from './pages/SignUp';
 function App() {
   return (
     <div className="App">
       
      <NavBar></NavBar>
+     <SignUp></SignUp>
     </div>
   );
 }
