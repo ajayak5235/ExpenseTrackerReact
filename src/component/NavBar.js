10c7d2db5a71aa70e7973c4417f031038bcfc949
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2024-03-06 23:59:49 +0530

    Update NavBar.js

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,20 +1,36 @@
-import {Container, Nav, NavLink, Navbar} from 'react-bootstrap'
-import myImage from '../logo/ball.webp'
-const NavBar = () =>{
-return<Navbar >
-   
-<Container>
 
-<Nav>
 
-    <NavLink>ExpenseTracker</NavLink>
-    <NavLink>Home</NavLink>
-    <NavLink>Products</NavLink>
-    <NavLink>About</NavLink>
-</Nav>
-</Container>
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink, useNavigate } from 'react-router-dom';
 
-</Navbar>
+const NavBar = () => {
+  
+  const navigate = useNavigate();
 
+  const logoutHandler = () =>{
+    localStorage.removeItem('token');
+    navigate('/login');
+  }
+
+  return (
+    <>
+      { (
+        <Navbar  className='bg-dark'>
+          <Container>
+            <Nav className="align-items-center">
+               <NavLink>ExpenseTracker</NavLink>
+               <NavLink className="mx-lg-2 mx-sm-3">Home</NavLink>
+               <NavLink className="mx-lg-2 mx-sm-3">Products</NavLink>
+               <NavLink className="mx-lg-2 mx-sm-3">About</NavLink>
+               <NavLink to='/login' className="mx-lg-2 mx-sm-3">Login</NavLink>
+               <button onClick={logoutHandler} className="btn btn-link mx-lg-2 mx-sm-3">Logout</button>
+            </Nav>
+          </Container>
+        </Navbar>
+      )}
+    </>
+  );
 }
+
 export default NavBar;
+
