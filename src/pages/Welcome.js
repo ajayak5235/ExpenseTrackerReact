4dbb6d5b9aa400c9dde6fe843d972b8b170c31eb
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2024-03-12 12:43:03 +0530

    Delete src directory

--- src/pages/Welcome.js
+++
@@ -1,6 +0,0 @@
-const Welcome = () =>{
-    return <div>
-        <p>Welcome To Expense Tracker!</p>
-    </div>
-}
-export default Welcome;
--- src/states/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import incdecReducer from './reducers/index'
-export default configureStore({
-    reducer:{
-        number: incdecReducer
-    }
-})
